9e13b073bdd7c76750c908e10f895ced64abb352
RobertoCH44 <rober_aletur@hotmail.com>
2021-04-28 16:11:35 -0500

    Billboard

---
+++ Tierra sintética/Assets/Systems/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera theCam;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(theCam.transform);
+    }
+}
